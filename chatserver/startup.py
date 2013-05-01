0658f08b0f959645755f0dffc178631cd7fee257
Arild Nilsen <arild.nilsen2@gmail.com>
2013-05-01 22:50:40 +0200

    Added which python version tested with

--- chatserver/startup.py
+++ chatserver/startup.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     user_registry_process.start()
     load_balancer_process = Process(target=load_balancer.main)
     load_balancer_process.start()
-    time.sleep(0.5)  # Simple solution for avoiding race conditions towards Pyro name server
+    time.sleep(1)  # Simple solution for avoiding race conditions towards Pyro name server
     for port in config.CHAT_SERVER_PORT_LIST:
         chat_server_process = Process(target=chat_server.main, args=(port, ))
         chat_server_process.start()
